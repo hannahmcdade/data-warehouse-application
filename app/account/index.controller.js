3e5ba1057032512a06199790420653067f135cd8
HannahMcDade <hmcdade01@qub.ac.uk>
2016-03-25 21:19:41 +0000

    Code tidy up - indentation

--- app/account/index.controller.js
+++ app/account/index.controller.js
@@ -1,9 +1,7 @@
 (function () {
     'use strict';
 
-    angular
-        .module('app')
-        .controller('Account.IndexController', Controller);
+    angular.module('app').controller('Account.IndexController', Controller);
 
     function Controller($window, UserService, FlashService) {
       // ViewModel - the controller instance variable
@@ -16,31 +14,27 @@
         initController();
 
         function initController() {
-            // Get the current user
-            UserService.GetCurrent().then(function (user) {
-                vm.user = user;
-            });
+          // Get the current user
+          UserService.GetCurrent().then(function (user) {
+              vm.user = user;
+          });
         }
 
         function saveUser() {
-            UserService.Update(vm.user)
-                .then(function () {
-                    FlashService.Success('User updated');
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
+          UserService.Update(vm.user).then(function () {
+            FlashService.Success('User updated');
+          }).catch(function (error) {
+            FlashService.Error(error);
+          });
         }
 
         function deleteUser() {
-            UserService.Delete(vm.user._id).then(function () {
-                    // Log the user out once profile has been deleted
-                    $window.location = '/login';
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
+          UserService.Delete(vm.user._id).then(function () {
+            // Log the user out once profile has been deleted
+            $window.location = '/login';
+          }).catch(function (error) {
+            FlashService.Error(error);
+          });
         }
     }
-
 })();
--- app/dataApi/resultsScript.js
+++ app/dataApi/resultsScript.js
@@ -1,3 +1,5 @@
+// from http://www.json-generator.com/
+
 [
   '{{repeat(20)}}',
   {
