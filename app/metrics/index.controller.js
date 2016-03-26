97e215679b4bd511a598b554e1e2fe90cebab0e8
HannahMcDade <hmcdade01@qub.ac.uk>
2016-03-26 14:24:21 +0000

    Extracting JSON data for pie chart in metrics page

--- app/metrics/index.controller.js
+++ app/metrics/index.controller.js
@@ -1,32 +1,52 @@
 (function(){
   'use strict';
 
-  angular.module('app').controller('Metrics.IndexController', ['$scope', 'UserService', 'chart.js', function($scope, UserService){
+  angular.module('app').controller('Metrics.IndexController', ['$scope', 'UserService',
+  function($scope, UserService){
     // ViewModel - the controller instance variable
     var vm = this;
-
     vm.user = null;
+    var status = 'status'
 
-    // $scope.results = (function() {
-    //   var results = null;
-    //   $.ajax({
-    //     'async': false,
-    //     'global': false,
-    //     'url': "dataApi/results.json",
-    //     'dataType': "json",
-    //     'success': function (data) {
-    //         results = data;
-    //     }
-    //   });
-    //   return results;
-    // })();
+    var results = (function() {
+      var results = null;
+      $.ajax({
+        'async': false,
+        'global': false,
+        'url': "dataApi/results.json",
+        'dataType': "json",
+        'success': function (data) {
+            results = data;
+        }
+      });
+      return results;
+    })();
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+    // Get each of the status types
+    var uniqueStatus = [];
+    angular.forEach(results, function(item) {
+      var value = item.status;
+      if(uniqueStatus.indexOf(value) === -1) {
+        uniqueStatus.push(value);
+      }
+    });
+
+    // Calculate the amount of each status types
+    var uniqueStatusCount = [];
+    var total = 0;
+    for (var i = 0; i < uniqueStatus.length; i++) {
+      for (var j = 0; j < results.length; j++) {
+        if (uniqueStatus[i] === results[j].status) {
+          total++;
+        }
+      }
+      uniqueStatusCount.push(total);
+      // Reset the total
+      total = 0;
+    }
+
+    $scope.labels = uniqueStatus;
+    $scope.data = uniqueStatusCount;
 
     $scope.onClick = function (points, evt) {
       console.log(points, evt);
