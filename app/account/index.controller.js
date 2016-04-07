634bc02fe6d1e904e38152fc63879007255aa16a
HannahMcDade <hmcdade01@qub.ac.uk>
2016-04-07 20:39:25 +0100

    Adding email and password validation to My Account

--- app/account/index.controller.js
+++ app/account/index.controller.js
@@ -9,9 +9,20 @@
 
         vm.user = null;
         vm.saveUser = saveUser;
-        vm.deleteUser = deleteUser;
 
         initController();
+
+        // function to validate a users email address
+        function validateEmail(email) {
+          var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return regex.test(email);
+        }
+
+        // function to validate a users password
+        function validatePassword(password) {
+          var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+          return regex.test(password);
+        }
 
         function initController() {
           // Get the current user
@@ -21,11 +32,21 @@
         }
 
         function saveUser() {
-          UserService.Update(vm.user).then(function () {
-            FlashService.Success('User updated');
-          }).catch(function (error) {
-            FlashService.Error(error);
-          });
+          // ensure a valid email address is input
+          if (!validateEmail(vm.user.email)) {
+            FlashService.Error('Email "' + vm.user.email + '" is not valid');
+          }
+          // ensure a valid password is input
+          else if (!validatePassword(vm.user.password)) {
+            FlashService.Error('Password must contain one uppercase, one lowercase, one digit and be at least 8 characters long');
+          } else {
+            // Server side validation - update user profile. Check for other errors.
+            UserService.Update(vm.user).then(function () {
+              FlashService.Success('User updated');
+            }).catch(function (error) {
+              FlashService.Error(error);
+            });
+          }
         }
 
         function deleteUser() {
